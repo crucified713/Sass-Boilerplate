b452720b7673f7d83eda99692db76a14b95f5308
Mike Li <mli@BB-DES-2.local>
2017-05-15 12:47:56 +1000

    Cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,6 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
 var gulpIf = require('gulp-if');
 var cssnano = require('gulp-cssnano');
